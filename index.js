f9e87ab30f751106404bf054014747d6f121144a
Himanshu Patil <patilhnofficial@gmail.com>
2021-08-10 08:50:16 +0530

    db conection

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 const express = require("express");
+const mongoose = require("mongoose");
 require("dotenv").config();
 const app = express();
-const port = process.env.PORT || 7400;
+const port = process.env.PORT || 5000;
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-
 app.use((req, res, next) => {
   res.send("Welcome to Express");
 });
-
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+mongoose.connect(
+  process.env.MONGODB_URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err, client) => {
+    if (err) return console.log(err);
+    console.log(`connected to db`);
+  }
+);
