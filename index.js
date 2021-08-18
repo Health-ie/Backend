e79176fe21e8ab5935c6a733d8b4abb03c515022
Himanshu Patil <patilhnofficial@gmail.com>
2021-08-18 20:30:22 +0530

    update

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+//
 const ErrorHandler = require("./utils/errorHandler");
 require("dotenv").config();
 const app = express();
--- routes/doctor.routes.js
+++ routes/doctor.routes.js
@@ -2,4 +2,7 @@ const express = require("express");
 const { hello } = require("../controllers/doctor.controller");
 const router = express.Router();
 router.get("/", hello);
+router.get("/vaibhav",(req,res)=>{
+    res.send("doctor vaibhav")
+})
 module.exports = router;
