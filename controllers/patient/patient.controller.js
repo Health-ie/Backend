5b292fac4f4f2f8d261dd46fbd00a806c8416071
Himanshu Patil <patilhnofficial@gmail.com>
2021-10-04 12:04:19 +0530

    update

--- controllers/patient/patient.controller.js
+++ controllers/patient/patient.controller.js
@@ -12,17 +12,34 @@ exports.doctorList = (req, res) => {
     .catch((err) => console.log(err));
 };
 exports.bookAppointment = async (req, res) => {
-
   let app = new Appointment(req.body);
-  await Doctor.findById(req.body.doctor_id).then(data=>app.doctor = data)
-  await Patient.findById(req.body.patient_id).then(data=>app.patient = data)
+  await Doctor.findById(req.body.doctor_id).then((data) => (app.doctor = data));
+  await Patient.findById(req.body.patient_id).then((data) => (app.patient = data));
   app.save(function (err, app) {
     if (err) return console.error(err);
     res.send(app);
   });
 };
-exports.myAppointments = (req,res) => {
+exports.myAppointments = (req, res) => {
   Appointment.find({ patient_id: req.body.id })
     .then((data) => res.send(data))
     .catch((err) => console.log(err));
+};
+exports.updateAppointment = (req, res) => {
+  Appointment.findByIdAndUpdate(req.body.appointment_id, req.body.data, function (err, docs) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(docs);
+    }
+  });
+};
+exports.deleteAppointment = (req, res) => {
+  Appointment.findOneAndDelete({ _id: req.body.appointment_id }, function (err, app) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Deleted Appointmemnt : ", app);
+    }
+  });
 };
--- routes/patient.routes.js
+++ routes/patient.routes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { hello, bookAppointment, myAppointments } = require("../controllers/patient/patient.controller");
+const { hello, bookAppointment, myAppointments,updateAppointment,deleteAppointment } = require("../controllers/patient/patient.controller");
 const { login, register, logout } = require("../controllers/patient/auth.controller");
 const pauth = require("../middleware/pauth");
 const { doctorList } = require("../controllers/patient/patient.controller");
@@ -14,4 +14,6 @@ router.get("/doctorlist",pauth,doctorList);
 router.post("/book",pauth,bookAppointment);
 router.post("/myappointments",pauth,myAppointments)
 router.get("/logout", logout);
+router.post("/appointment/update",pauth,updateAppointment)
+router.post("/appointment/delete",pauth,deleteAppointment)
 module.exports = router;
