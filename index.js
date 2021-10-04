73590279350dafcda2ff4777f7a135ad447feb1d
Himanshu Patil <patilhnofficial@gmail.com>
2021-10-04 15:25:08 +0530

    updates

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ require("./config/database").connect();
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,Authorization,x-access-origin");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,Authorization,x-access-token");
   next();
 });
 
